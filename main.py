6fbdbc98efa59faded5f86f9891aa20fef5871fa
Ross Fisher <zorrobyte@gmail.com>
2022-05-06 21:22:40 -0400

    FPS

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 # import required libraries
 from vidgear.gears import VideoGear
 import cv2
+import numpy as np
+import time
 
 # define suitable tweak parameters for your stream.
 options = {
-    "CAP_PROP_FRAME_WIDTH": 853,  # resolution 320x240
-    "CAP_PROP_FRAME_HEIGHT": 480,
-    "CAP_PROP_FPS": 20,  # frame rate 60fps
+    "CAP_PROP_FRAME_WIDTH": 1920,  # resolution 320x240
+    "CAP_PROP_FRAME_HEIGHT": 1080,
+    "CAP_PROP_FPS": 60,  # frame rate 60fps
     "CAP_PROP_AUTOFOCUS": 0,
     "CAP_PROP_FOCUS": 0,
 }
@@ -18,10 +20,16 @@ stream1 = VideoGear(source=0, logging=True, **options).start()
 #stream2 = VideoGear(source=1, logging=True).start()
 
 # define and start the stream on second source ( For e.g #1 index device)
-stream3 = VideoGear(source=2, logging=True, **options).start()
+stream3 = VideoGear(source=1, logging=True, **options).start()
 
 # define and start the stream on second source ( For e.g #1 index device)
-stream4 = VideoGear(source=3, logging=True, **options).start()
+stream4 = VideoGear(source=2, logging=True, **options).start()
+
+# used to record the time when we processed last frame
+prev_frame_time = 0
+
+# used to record the time at which we processed current frame
+new_frame_time = 0
 
 # infinite loop
 while True:
@@ -42,6 +50,29 @@ while True:
     if frameA is None or frameC is None:
         # if True break the infinite loop
         break
+
+    # font which we will be using to display FPS
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    # time when we finish processing for this frame
+    new_frame_time = time.time()
+
+    # Calculating the fps
+
+    # fps will be number of frame processed in given time frame
+    # since their will be most of time error of 0.001 second
+    # we will be subtracting it to get more accurate result
+    fps = 1 / (new_frame_time - prev_frame_time)
+    prev_frame_time = new_frame_time
+
+    # converting the fps into integer
+    fps = int(fps)
+
+    # converting the fps to string so that we can display it on frame
+    # by using putText function
+    fps = str(fps)
+
+    # putting the FPS count on the frame
+    cv2.putText(frameA, fps, (7, 70), font, 3, (100, 255, 0), 3, cv2.LINE_AA)
 
     # do something with both frameA and frameB here
     cv2.imshow("Output Frame1", frameA)
